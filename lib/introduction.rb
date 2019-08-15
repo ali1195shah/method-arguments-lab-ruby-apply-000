4a2152f01d4626a990cc739e1f9c4b37d3306d3c
Ali Shah <ali1195shah@gmail.com>
2019-08-15 16:14:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name_str, language_str)
-  puts "Hi, my name is #{name_str}\n and I am learning to program in #{language_str}.\n"
+  puts "Hi, my name is #{name_str} \n and I am learning to program in #{language_str}.\n"
 end
